00b56087c5f1c254b251c554504dbedc414ccf0e
xcash-rocks <dev@xcash.rocks>
2022-01-27 18:32:51 +0100

    refactor: rename modules to pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,49 +2,49 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 // components imports
-//import { HomeComponent } from './modules/home/home.component';
-//import { ProjectComponent } from './modules/project/project.component';
-//import { TeamComponent } from './modules/team/team.component';
-//import { GetstartedComponent } from './modules/getstarted/getstarted.component';
-//import { WalletComponent } from './modules/wallet/wallet.component';
-//import { RessourcesComponent } from './modules/ressources/ressources.component';
-//import { ContributeComponent } from './modules/contribute/contribute.component';
-import { NotFoundComponent } from './modules/not-found/not-found.component';
+//import { HomeComponent } from './pages/home/home.component';
+//import { ProjectComponent } from './pages/project/project.component';
+//import { TeamComponent } from './pages/team/team.component';
+//import { GetstartedComponent } from './pages/getstarted/getstarted.component';
+//import { WalletComponent } from './pages/wallet/wallet.component';
+//import { RessourcesComponent } from './pages/ressources/ressources.component';
+//import { ContributeComponent } from './pages/contribute/contribute.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./modules/home/home.module')
+    loadChildren: () => import('./pages/home/home.module')
       .then(mod => mod.HomeModule)
   },
   {
     path: 'project',
-    loadChildren: () => import('./modules/project/project.module')
+    loadChildren: () => import('./pages/project/project.module')
       .then(mod => mod.ProjectModule)
   },
   {
     path: 'team',
-    loadChildren: () => import('./modules/team/team.module')
+    loadChildren: () => import('./pages/team/team.module')
       .then(mod => mod.TeamModule)
   },
   {
     path: 'get-started',
-    loadChildren: () => import('./modules/getstarted/getstarted.module')
+    loadChildren: () => import('./pages/getstarted/getstarted.module')
       .then(mod => mod.GetstartedModule)
   },
   {
     path: 'wallet',
-    loadChildren: () => import('./modules/wallet/wallet.module')
+    loadChildren: () => import('./pages/wallet/wallet.module')
       .then(mod => mod.WalletModule)
   },
   {
     path: 'ressources',
-    loadChildren: () => import('./modules/ressources/ressources.module')
+    loadChildren: () => import('./pages/ressources/ressources.module')
       .then(mod => mod.RessourcesModule)
   },
   {
     path: 'contribute',
-    loadChildren: () => import('./modules/contribute/contribute.module')
+    loadChildren: () => import('./pages/contribute/contribute.module')
       .then(mod => mod.ContributeModule)
   },
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,24 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-// import { HomeComponent } from './modules/home/home.component';
-// import { ProjectComponent } from './modules/project/project.component';
-// import { TeamComponent } from './modules/team/team.component';
-// import { GetstartedComponent } from './modules/getstarted/getstarted.component';
-// import { WalletComponent } from './modules/wallet/wallet.component';
-// import { ContributeComponent } from './modules/contribute/contribute.component';
-// import { RessourcesComponent } from './modules/ressources/ressources.component';
-import { NotFoundComponent } from './modules/not-found/not-found.component';
+// import { HomeComponent } from './pages/home/home.component';
+// import { ProjectComponent } from './pages/project/project.component';
+// import { TeamComponent } from './pages/team/team.component';
+// import { GetstartedComponent } from './pages/getstarted/getstarted.component';
+// import { WalletComponent } from './pages/wallet/wallet.component';
+// import { ContributeComponent } from './pages/contribute/contribute.component';
+// import { RessourcesComponent } from './pages/ressources/ressources.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { ScrollToTopComponent } from './widgets/scroll-to-top/scroll-to-top.component';
-// import { HomeModule } from './modules/home/home.module';
-// import { ProjectModule } from './modules/project/project.module';
-// import { TeamModule } from './modules/team/team.module';
-// import { GetstartedModule } from './modules/getstarted/getstarted.module';
-// import { WalletModule } from './modules/wallet/wallet.module';
-// import { ContributeModule } from './modules/contribute/contribute.module';
-// import { RessourcesModule } from './modules/ressources/ressources.module';
+// import { HomeModule } from './pages/home/home.module';
+// import { ProjectModule } from './pages/project/project.module';
+// import { TeamModule } from './pages/team/team.module';
+// import { GetstartedModule } from './pages/getstarted/getstarted.module';
+// import { WalletModule } from './pages/wallet/wallet.module';
+// import { ContributeModule } from './pages/contribute/contribute.module';
+// import { RessourcesModule } from './pages/ressources/ressources.module';
 
 @NgModule({
   declarations: [
