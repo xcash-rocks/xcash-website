c5fbe4695507a2ff9f5ba5ba60ce1f4f2b3c2c42
xcash-rocks <dev@xcash.rocks>
2022-01-28 18:34:32 +0100

    fix: undo jquery injection test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-
-declare var $: any;
-
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'X-Cash | Private Cryptocurrency for the Web3.0';
   version = 'X-Cash is the Web3’s private computing Platform.';
-
-  ngOnInit(){
-     //  $('button').click(function(){
-     //    alert('Wass up!'');
-     // });
-   }
 }
-
-//
-//
-// export class AppComponent {
-//   title = 'X-Cash | Private Cryptocurrency for the Web3.0';
-//   version = 'X-Cash is the Web3’s private computing Platform.';
-// }
