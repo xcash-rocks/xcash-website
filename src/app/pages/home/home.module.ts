36384c478a7922b95a4e54fd8947df9b239f67d1
xcash-rocks <dev@xcash.rocks>
2022-02-01 23:11:45 +0100

    add ngx-lottie

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgxLottieViewModule } from 'ngx-lottie-view';
 
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
@@ -7,7 +8,8 @@ import { HomeRoutingModule } from './home-routing.module';
 @NgModule({
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    NgxLottieViewModule
   ],
   exports: [
     HomeComponent
