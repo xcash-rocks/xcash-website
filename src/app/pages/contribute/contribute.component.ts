ba7efe2cf935e38d9ea45a9bad03a7c0db938d78
xcash-rocks <dev@xcash.rocks>
2022-02-08 15:51:48 +0100

    feat: add toggleDiv show/hide

--- src/app/pages/contribute/contribute.component.ts
+++ src/app/pages/contribute/contribute.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContributeComponent implements OnInit {
 
+  subSectionName:string;
+  divTab = '';
+
+  divToggle(index){
+    if(index == this.divTab) {
+      this.divTab = '';
+    }else{
+      this.divTab =index;
+    }
+    console.log('divTab',this.divTab);
+    console.log('index',index);
+  }
+
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
 
 }
