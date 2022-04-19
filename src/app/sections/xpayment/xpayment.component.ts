f3e9545db28c2eb44985f7f8449b71bb12d78891
xcash-rocks <dev@xcash.rocks>
2022-04-19 21:31:52 +0200

    fix: xpayment section responsive behavior

--- src/app/sections/xpayment/xpayment.component.ts
+++ src/app/sections/xpayment/xpayment.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-xpayment',
   templateUrl: './xpayment.component.html',
-  styleUrls: ['../../pages/home/home.component.css']
+  styleUrls: ['./xpayment.component.css']
 })
 export class XpaymentComponent implements OnInit {
 
