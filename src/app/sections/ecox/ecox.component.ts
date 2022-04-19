0b6a192e9bd10dfa2578f8dc98c0a1a92ddea0a8
xcash-rocks <dev@xcash.rocks>
2022-04-19 23:08:16 +0200

    fix: ecox section responsive behavior

--- src/app/sections/ecox/ecox.component.ts
+++ src/app/sections/ecox/ecox.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-ecox',
   templateUrl: './ecox.component.html',
-  styleUrls: ['../../pages/home/home.component.css']
+  styleUrls: ['./ecox.component.css']
 })
 export class EcoxComponent implements OnInit {
 
