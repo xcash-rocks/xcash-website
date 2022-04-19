d3e1a7ae745e8d8cd7821df0f93eb1d4fa67c7f6
xcash-rocks <dev@xcash.rocks>
2022-04-19 23:34:42 +0200

    fix: xbank section responsive behavior

--- src/app/sections/xbank/xbank.component.ts
+++ src/app/sections/xbank/xbank.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-xbank',
   templateUrl: './xbank.component.html',
-  styleUrls: ['../../pages/home/home.component.css']
+  styleUrls: ['./xbank.component.css']
 })
 export class XbankComponent implements OnInit {
 
