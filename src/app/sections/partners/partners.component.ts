5936446625a126b4fae68793a39bc97a9b52e327
xcash-rocks <dev@xcash.rocks>
2022-04-20 20:09:47 +0200

    fix: partners section responsive behavior

--- src/app/sections/partners/partners.component.ts
+++ src/app/sections/partners/partners.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-partners',
   templateUrl: './partners.component.html',
-  styleUrls: ['../../pages/home/home.component.css']
+  styleUrls: ['./partners.component.css']
 })
 export class PartnersComponent implements OnInit {
 
