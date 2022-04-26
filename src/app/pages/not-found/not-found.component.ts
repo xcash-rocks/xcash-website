32220d4e185abeb7e1de4ae237b0d2577fa67141
Caco <xcashbuyer@protonmail.com>
2022-04-26 23:51:54 +0200

    - put the discover page in an overlay and added this to the 404 page

--- src/app/pages/not-found/not-found.component.ts
+++ src/app/pages/not-found/not-found.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Overlay, OverlayRef } from "@angular/cdk/overlay";
+import { ComponentPortal } from "@angular/cdk/portal";
+import { DiscoverComponent } from 'src/app/sections/discover/discover.component';
+
 
 @Component({
   selector: 'app-not-found',
@@ -7,7 +11,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotFoundComponent implements OnInit {
 
-  constructor() { }
+  overlayRef: OverlayRef | undefined;
+
+  public show:boolean = false;
+  public buttonName:any = 'Discover X-Cash';
+
+  constructor( private overlay: Overlay) { }
+
+  open() {
+    this.overlayRef = this.overlay.create();
+    const componentPortal = new ComponentPortal(DiscoverComponent);
+    this.overlayRef.addPanelClass("example-list");
+    this.overlayRef.attach(componentPortal);
+}
+
+
+toggle() {
+  this.show = !this.show;
+  if(this.show)
+  this.buttonName = "Keep Discovering because CDK overlay doesn't have a close method";
+else
+  this.buttonName = "Discover X-Cash";
+  this.open()
+}
 
   ngOnInit(): void {
   }
