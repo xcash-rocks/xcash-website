bd6ac6195cbd965ed0dc534860d969d69c7598cc
xcash-rocks <dev@xcash.rocks>
2022-04-29 00:36:19 +0200

    refactor layout and responsive behavior

--- src/app/sections/namespace/namespace.component.ts
+++ src/app/sections/namespace/namespace.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-namespace',
   templateUrl: './namespace.component.html',
-  styleUrls: ['../../pages/home/home.component.css']
+  styleUrls: ['./namespace.component.css']
 })
 export class NamespaceComponent implements OnInit {
 
